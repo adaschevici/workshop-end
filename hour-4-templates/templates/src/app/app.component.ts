68fee24470519e853868c260ef87ca6fba38285a
adaschevici <a.daschevici@gmail.com>
2018-02-06 23:07:41 +0000

    Add fancier style

--- hour-4-templates/templates/src/app/app.component.ts
+++ hour-4-templates/templates/src/app/app.component.ts
@@ -15,6 +15,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string;
+  isDisabled: boolean;
 
   constructor() {
     this.title = 'App Title';
