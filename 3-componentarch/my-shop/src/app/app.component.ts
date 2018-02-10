78b22eb038d9b7b48678fde68ed9450a327beb58
adaschevici <a.daschevici@gmail.com>
2018-02-10 12:17:07 +0000

    Play around with some styling on the main app component

--- 3-componentarch/my-shop/src/app/app.component.ts
+++ 3-componentarch/my-shop/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="app-root">
+      {{ title }}
+    </div>
+  `,
 })
 export class AppComponent {
   title: string;
