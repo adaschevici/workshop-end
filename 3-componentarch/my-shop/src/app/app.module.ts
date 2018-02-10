1fe71fb61fef943131d8617da8d5d125bca9117a
adaschevici <a.daschevici@gmail.com>
2018-02-10 13:18:25 +0000

    Remove old component:

--- 3-componentarch/my-shop/src/app/app.module.ts
+++ 3-componentarch/my-shop/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
   ],
   imports: [
     BrowserModule
