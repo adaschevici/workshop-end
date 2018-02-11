2cc0ef9520b4a95c57192aa639a8157261a2e8cd
adaschevici <a.daschevici@gmail.com>
2018-02-11 20:29:09 +0000

    Split list into dumb components

--- 6-ngmodules-components/my-shop/src/app/product-list/product-detail/product-detail.component.ts
+++ 6-ngmodules-components/my-shop/src/app/product-list/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Product } from '../models/product.interface';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  @Input()
+  detail: Product;
+
+  @Input()
+  idx: number;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.detail);
   }
 
 }
